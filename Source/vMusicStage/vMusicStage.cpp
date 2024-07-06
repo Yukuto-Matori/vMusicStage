7ae831b6ed488bb100f7f5b9fa63a203fa17a6dd
Yukuto-Matori <yukuto.matori@gmail.com>
2024-07-06 13:31:51 +0900

    初期 UE5 プロジェクト作成

---
+++ Source/vMusicStage/vMusicStage.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "vMusicStage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, vMusicStage, "vMusicStage" );
